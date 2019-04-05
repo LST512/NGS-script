dd86882648ec539cb07e5e14994eed2cfeb678ae
LST512 <838395676@qq.com>
2019-04-05 15:03:23 +0800

    add

--- git_add_commit_push.sh
+++ git_add_commit_push.sh
@@ -6,10 +6,10 @@ echo -e "\e[1;31m..Start..git.....\e[0m\n"
 echo -e "##############################\n"
 
 
-
-if [ ! -n $1 ];then
+#if [ ! -n $1 ]; 不传递$1，报错
+if [ ! $1 ];then
     echo -e "\e[1;43m No add git_commit content \e[0m\n"
-    git add --all && git commit -m "add content" && git push origin master
+    git add --all && git commit -m "add" && git push origin master
 else
     echo -e "\e[1;41m add git commit content:\e[0m$1\n"
     git add --all && git commit -m "$1" && git push origin master
