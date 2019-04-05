9ef62b4efc9b86190e84da5c2ab50eb98385536e
LST512 <838395676@qq.com>
2019-04-05 14:56:45 +0800

    git_auto.sh

--- git_add_commit_push.sh
+++ git_add_commit_push.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-echo -e "\e[1;31m..Start..git.....\e[0m\n"
+
 date
+echo -e "\e[1;31m..Start..git.....\e[0m\n"
 echo -e "##############################\n"
 
+
+
 if [ ! -n $1 ];then
-    git add --all && git commit -m "$1" && git push origin master
-else
+    echo "No add git_commit content"
     git add --all && git commit -m "add content" && git push origin master
+else
+    echo "add git commit content:$1"
+    git add --all && git commit -m "$1" && git push origin master
 fi
+
+
+
 echo -e "##############################\n"
 echo -e "\e[1;32m..ok.....finish git.....\e[1;32m\n"
 date
