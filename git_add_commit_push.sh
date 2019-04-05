a5ac1f36531a70412a91e3ba377e73d4b7f630de
LST512 <838395676@qq.com>
2019-04-05 14:45:30 +0800

    add content

--- git_add_commit_push.sh
+++ git_add_commit_push.sh
@@ -4,7 +4,7 @@ date
 echo "start........"
 echo "##############################\n"
 
-if [$1 != "" ];then
+if [ ! -n $1 ];then
     git add --all && git commit -m "$1" && git push origin master
 else
     git add --all && git commit -m "add content" && git push origin master
